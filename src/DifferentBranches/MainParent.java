31111138d138cfbf5e0a8cb349ad45bbf7d50019
Sergeev <SergeevSA1978@ya.ru>
2020-05-06 13:56:34 +0300

    Работа с разными ветками на GitHub.

---
+++ src/DifferentBranches/MainParent.java
@@ -0,0 +1,9 @@
+package DifferentBranches;
+
+// Настройка работы с разными ветками в репозитарии на GitHub.
+
+public class MainParent {
+    public static void main(String[] args) {
+        System.out.println("06.05.2020 г. (13:54) создан класс MainParent - главный родитель(корень). ");
+    }
+}
