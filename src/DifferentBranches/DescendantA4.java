f7857267531b43f87ccb876b358bd903e7f9af77
Sergeev <SergeevSA1978@ya.ru>
2020-05-06 16:43:00 +0300

    Основная ветка 'A'. Четвертое изменение. 06.05.2020.

---
+++ src/DifferentBranches/DescendantA4.java
@@ -0,0 +1,11 @@
+package DifferentBranches;
+
+//Четвертое изменение основной ветви 'A'.
+
+public class DescendantA4 {
+
+    void descendantA4() {
+        System.out.println("06.05.2020 г. (16:40) создан класс DescendantA4");
+        System.out.println("Ветка А (основная). Четвертое изменение.");
+    }
+}
