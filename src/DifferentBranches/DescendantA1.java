9e741a27b4e0c92a2e89b27d082a31505a9f126d
Sergeev <SergeevSA1978@ya.ru>
2020-05-06 14:11:31 +0300

    Создана ветка 'А' (06.05.2020).

---
+++ src/DifferentBranches/DescendantA1.java
@@ -0,0 +1,10 @@
+package DifferentBranches;
+
+//Создаем ветвь "A" (ветвь номер 1).
+
+public class DescendantA1 {
+    void descendantA1() {
+        System.out.println("06.05.2020 г. (14:05) создан класс DescendantA1");
+        System.out.println("Создается первая ветка (А) дерева Проекта GitHub.");
+    }
+}
