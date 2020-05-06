2aae177f6dffc58f732291690e2159ba53de4af1
Sergeev <SergeevSA1978@ya.ru>
2020-05-06 16:24:44 +0300

    Третья ветка B_C. Ответвление ветки B. Первое изменение. 06.05.2020.

---
+++ src/DifferentBranches/DescendantB3_C1.java
@@ -0,0 +1,11 @@
+package DifferentBranches;
+
+// Создана третья ветка 'B_C'. Ответвление второй ветки 'B'. Первое изменение.
+
+public class DescendantB3_C1 {
+
+    void descendantB3_C1() {
+        System.out.println("06.05.2020 г. (16:21) создан класс DescendantB3_C1");
+        System.out.println("Третья ветка B_C. Ответвление ветки B. Первое изменение.");
+    }
+}
