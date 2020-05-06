b46b503ec1492b39a9c763e64defbd51eefb3668
Sergeev <SergeevSA1978@ya.ru>
2020-05-06 14:28:12 +0300

    push после переключения в репозитариии на новую ветку.

---
+++ src/DifferentBranches/DescendantB1.java
@@ -0,0 +1,10 @@
+package DifferentBranches;
+
+//Создаем класс после создания в репозитарии новой ветви и переключения на нее.
+
+public class DescendantB1 {
+    void descendantB1() {
+        System.out.println("06.05.2020 г. (14:25) создан класс DescendantB1");
+        System.out.println("Делаем push после переключения в репозитариии на новую ветку.");
+    }
+}
