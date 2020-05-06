6be4caeda4521c7655398412b2298129db4280b5
Sergeev <SergeevSA1978@ya.ru>
2020-05-06 16:35:44 +0300

    Четвертая ветка A_D. Ответвление ветки A. Первое изменение 06.05.2020.

---
+++ src/DifferentBranches/DescendantA3_D1.java
@@ -0,0 +1,11 @@
+package DifferentBranches;
+
+//Создана четвертая ветка 'D'. Ответвление ветки 'А'. Первое изменение четвертой ветки 'D'.
+
+public class DescendantA3_D1 {
+
+    void descendantA3_D1() {
+        System.out.println("06.05.2020 г. (16:33) создан класс DescendantA3_D1");
+        System.out.println("Ветка D (четвертая). Первое изменение.");
+    }
+}
