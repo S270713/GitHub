81a4cac2f2929cb1e40f1437ba43e37510c2fb6c
Sergeev <SergeevSA1978@ya.ru>
2020-05-06 15:53:14 +0300

    Вторая ветка 'B'. Второе изменение.

---
+++ src/DifferentBranches/DescendantB2.java
@@ -0,0 +1,11 @@
+package DifferentBranches;
+
+//Второе изменение дополнительной ветви 'B.
+
+public class DescendantB2 {
+
+    void descendantB2() {
+        System.out.println("06.05.2020 г. (15:51) создан класс DescendantB2");
+        System.out.println("Ветка B (вторая). Второе изменение.");
+    }
+}
