19838da50cdb2485347fa687a9f20b7e4c747e7a
Sergeev <SergeevSA1978@ya.ru>
2020-05-06 16:00:48 +0300

    Основная ветка 'A'. Третье изменение.

---
+++ src/DifferentBranches/DescendantA3.java
@@ -0,0 +1,11 @@
+package DifferentBranches;
+
+//Третье изменение основной ветви 'A'.
+
+public class DescendantA3 {
+
+    void descendantA3() {
+        System.out.println("06.05.2020 г. (15:58) создан класс DescendantA3");
+        System.out.println("Ветка А (основная). Третье изменение.");
+    }
+}
