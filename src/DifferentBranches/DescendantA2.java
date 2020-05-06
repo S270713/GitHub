266daedf89e65d588ba8afd47bd2d4ca651fbe67
Sergeev <SergeevSA1978@ya.ru>
2020-05-06 15:47:25 +0300

    Основная ветка 'A'. Второе изменение.

---
+++ src/DifferentBranches/DescendantA2.java
@@ -0,0 +1,11 @@
+package DifferentBranches;
+
+//Второе изменение основной ветви 'A'.
+
+public class DescendantA2 {
+
+    void descendantA2() {
+        System.out.println("06.05.2020 г. (15:44) создан класс DescendantA2");
+        System.out.println("Ветка А (основная). Второе изменение.");
+    }
+}
