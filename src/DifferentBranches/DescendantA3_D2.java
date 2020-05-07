1c809cabc17bf510304364d0c670323f272f8d52
Sergeev <SergeevSA1978@ya.ru>
2020-05-07 15:21:16 +0300

    Четвертая ветка A_D. Ответвление ветки A в т. А3. Второе изменение 07.05.2020.

---
+++ src/DifferentBranches/DescendantA3_D2.java
@@ -0,0 +1,11 @@
+package DifferentBranches;
+
+//Четвертая ветка 'D'. Ответвление ветки 'А' в точке А3. Второе изменение четвертой ветки 'D'.
+
+public class DescendantA3_D2 {
+
+    void descendantA3_D2() {
+        System.out.println("07.05.2020 г. создан класс DescendantA3_D2");
+        System.out.println("Ветка D (четвертая). Второе изменение.");
+    }
+}
