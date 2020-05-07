b861bd63620a637162e84dbdd6e48af9a4618ae9
Sergeev <SergeevSA1978@ya.ru>
2020-05-07 15:31:12 +0300

    Пятая ветка A_D_E. Ответвление ветки A_D. Первое изменение 07.05.2020.

---
+++ src/DifferentBranches/DescendantA3_D2_E1.java
@@ -0,0 +1,11 @@
+package DifferentBranches;
+
+//Пятая ветка 'E'. Ответвление ветки 'А_D' в точке A3_D2. Первое изменение пятой ветки 'E'.
+
+public class DescendantA3_D2_E1 {
+
+    void descendantA3_D2_E1() {
+        System.out.println("07.05.2020 г. создан класс DescendantA3_D2_E1");
+        System.out.println("Ветка E (пятая). Создание.");
+    }
+}
