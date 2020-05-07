038ce395d71a03cf020a0ceec6cc0cb7267993dd
Sergeev <SergeevSA1978@ya.ru>
2020-05-07 15:00:51 +0300

    Основная ветка 'A'. Пятое изменение. 07.05.2020.

---
+++ src/DifferentBranches/DescendantA5.java
@@ -0,0 +1,11 @@
+package DifferentBranches;
+
+//Пятое изменение основной ветви 'A'.
+
+public class DescendantA5 {
+
+    void descendantA5() {
+        System.out.println("07.05.2020 г. создан класс DescendantA5");
+        System.out.println("Ветка А (основная). Пятое изменение.");
+    }
+}
